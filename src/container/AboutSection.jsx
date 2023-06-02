bbb317dff985cddafa20ecdda8d9110bce072945
NaufalRafianto <naufl.rafianto21@gmail.com>
2023-06-02 15:15:41 +0700

    feat: tanbah header

--- src/container/AboutSection.jsx
+++ src/container/AboutSection.jsx
@@ -2,5 +2,11 @@ import { merge } from '@/utils/merge'
 import React from 'react'
 
 export default function AboutSection({ id, className }) {
-    return <div id={id} className={merge('min-h-screen w-screen', className)}></div>
+    return (
+        <div id={id} className={merge('min-h-screen w-screen', className)}>
+            <div>
+                <div className="h-[31.25rem] w-[31.25rem] rounded-tl-full bg-cyellow" />
+            </div>
+        </div>
+    )
 }
