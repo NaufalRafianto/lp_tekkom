e5df8ccdf4a78975b26fa0b3d5bcae9d926e1c11
NaufalRafianto <naufl.rafianto21@gmail.com>
2023-06-02 14:46:44 +0700

    feat: add colors

--- src/container/AboutSection.jsx
+++ src/container/AboutSection.jsx
@@ -2,9 +2,5 @@ import { merge } from '@/utils/merge'
 import React from 'react'
 
 export default function AboutSection({ id, className }) {
-    return (
-        <div id={id} className={merge('min-h-screen w-screen', className)}>
-            AboutSection
-        </div>
-    )
+    return <div id={id} className={merge('min-h-screen w-screen', className)}></div>
 }
