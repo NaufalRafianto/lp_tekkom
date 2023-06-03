82a0441bb28ab1b116a74c406c7f26fd1b7a7ecf
Reza Ali Nirwansyah <rezaalinirwansyah@gmail.com>
2023-06-03 23:15:48 +0700

    feat(menyelesaikan main): menyelesaikan main

--- src/container/MainSection.jsx
+++ src/container/MainSection.jsx
@@ -4,23 +4,23 @@ import React from 'react'
 export default function MainSection({ id, className }) {
     return (
         <div id={id} className={merge('min-h-screen w-screen', className)}>
-            <div className=" mx-4 w-[95vw] min-h-[80vh] pl-14 bg-cyellow rounded-md shadow-md ] bg-[url('/assets/images/computer.png')] bg-no-repeat bg-right ">
-                <div className='pt-11 mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white'>
+            <div className=" ] mx-4 min-h-[80vh] w-[95vw] rounded-md bg-cyellow bg-[url('/assets/images/computer.png')] bg-right bg-no-repeat pl-14 shadow-md ">
+                <div className="mb-4 pt-11 text-4xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
                     <h1>COMPUTER</h1>
                     <h1>ENGINEERING</h1>
                 </div>
 
-                <div className='py-11 text-4xl font-extrabold dark:text-white'>
+                <div className="py-11 text-4xl font-extrabold dark:text-white">
                     <h2>Where Creativity Meets</h2>
                     <h2>Future Technology</h2>
                 </div>
 
-                <div className='py-11'>
-                    <button className='text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700'>
+                <div className="py-11">
+                    <button className="mb-2 mr-2 rounded-lg bg-gray-800 px-5 py-2.5 text-sm font-medium text-white hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700">
                         button
                     </button>
 
-                    <button className='text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700'>
+                    <button className="mb-2 mr-2 rounded-lg border border-gray-300 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700">
                         button
                     </button>
                 </div>
